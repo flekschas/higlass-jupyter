12db552a239c9664f6a4a5254898d246e05760bb
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-09-13 17:00:43 -0400

    Working on adding external js files

--- setup.py
+++ setup.py
@@ -67,7 +67,8 @@ class NPM(Command):
 
     targets = [
         os.path.join(here, 'jupyter_higlass', 'static', 'extension.js'),
-        os.path.join(here, 'jupyter_higlass', 'static', 'index.js')
+        os.path.join(here, 'jupyter_higlass', 'static', 'index.js'),
+        os.path.join(here, 'jupyter_higlass', 'static', 'higlass-multivec.js')
     ]
 
     def initialize_options(self):
